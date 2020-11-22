0e600c30436f733be20e422aba8861894089c021
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-11-22 18:23:40 -0300

    O tipo Enum

---
+++ src/A0009-tipo-enum/A0009-tipo-enum.ts
@@ -0,0 +1,19 @@
+enum Cores {
+  VERMELHO = 10, // 10
+  AZUL = 100, // 100
+  AMARELO = 200, // 200
+}
+
+enum Cores {
+  ROXO = 'ROXO',
+  VERDE = 201,
+  ROSA,
+}
+
+export function escolhaACor(cor: Cores): void {
+  console.log(Cores[cor]);
+}
+
+escolhaACor(
+  123456,
+); /* o typescript permite qualquer coisa aqui, o que é ruim */
