13c8123b079d4ecc39aae4870b6020814248480c
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-11-22 16:50:10 -0300

    O tipo any

--- src/A000-type-annotation/A000-type-annotation.ts
+++ src/A000-type-annotation/A000-type-annotation.ts
@@ -6,7 +6,6 @@ let idade: number = 0b1010; // 10, 1.57, -5.55, 0xf00d, 0b1010, 0o7744
 let adulto: boolean = true; // true ou false
 let simbolo: symbol = Symbol('qualquer-symbol'); // symbol
 // let big: bigint = 10n; // bigint
-/* É bom especificar o tipo só se ver que tá Any */
 
 // Arrays
 let arrayDeNumeros: Array<number> = [1, 2, 3];
