20fe94d263d28b60fcd831fb8d1b431ddf67bf1b
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-11-23 14:50:59 -0300

    Classes e Herança

--- src/Classe/classes.ts
+++ src/Classe/classes.ts
@@ -6,7 +6,26 @@ class UserAccount {
     this.name = name;
     this.age = age;
   }
+
+  logDetails(): void {
+    console.log(`The player ${this.name} is ${this.age} years old`);
+  }
 }
 
+class CharAccount extends UserAccount {
+  nickname: string;
+  level: number;
+
+  constructor(name: string, age: number, nickname: string, level: number) {
+    super(name, age); /* ali dentro do parenteses do construtor nickname e level pode ser qualquer nome*/
+    this.nickname = nickname;
+    this.level = level;
+  }
+}
 const will = new UserAccount('Hugo', 30);
 console.log(will);
+
+const john = new CharAccount('John', 45, 'johnmaster', 45);
+console.log(john.name);
+console.log(john);
+john.logDetails(); /* Herança permitiu que usasse o método da classe pai */
